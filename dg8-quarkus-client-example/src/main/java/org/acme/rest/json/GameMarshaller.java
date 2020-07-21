ea1e5861821792c216cf0325aed1ec1bd73417b8
sshaaf <sshaaf@redhat.com>
2020-07-21 15:55:25 +0200

    ported from dg8-workshop repo, seperating repos..

---
+++ dg8-quarkus-client-example/src/main/java/org/acme/rest/json/GameMarshaller.java
@@ -0,0 +1,16 @@
+package org.acme.rest.json;
+
+import java.io.IOException;
+
+import org.infinispan.protostream.MessageMarshaller;
+
+public class GameMarshaller implements MessageMarshaller<Game>{
+
+    // Override readFrom
+
+    // Override writeTo
+
+    // Override getJavaClass
+
+    // Override getTypeName
+}
---
+++ dg8-quarkus-client-example/src/test/java/org/acme/rest/json/GameResourceTest.java
@@ -0,0 +1,16 @@
+package org.acme.rest.json;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+import javax.ws.rs.core.MediaType;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class GameResourceTest {
+
+}
