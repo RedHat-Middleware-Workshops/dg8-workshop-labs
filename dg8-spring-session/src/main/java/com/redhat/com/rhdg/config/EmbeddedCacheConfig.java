95c5468d750249cd64b7632647edea6bb460aee9
Eric Deandrea <eric.deandrea@gmail.com>
2020-08-07 13:47:44 -0400

    Update for externalizing session lab

---
+++ dg8-spring-session/src/main/java/com/redhat/com/rhdg/config/EmbeddedCacheConfig.java
@@ -0,0 +1,26 @@
+package com.redhat.com.rhdg.config;
+
+import org.infinispan.commons.marshall.JavaSerializationMarshaller;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.spring.embedded.session.configuration.EnableInfinispanEmbeddedHttpSession;
+import org.infinispan.spring.starter.embedded.InfinispanGlobalConfigurer;
+
+import com.redhat.com.rhdg.CacheListener;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import infinispan.autoconfigure.embedded.InfinispanCacheConfigurer;
+
+// TODO: Add an Infinispan annotation here
+@Configuration
+public class EmbeddedCacheConfig {
+  
+  // TODO: Add cacheConfigurer method here
+
+
+  // TODO: Add globalCustomizer method here
+  
+}
