b6df0352292fe25e8a50786f89e0e0595538ec83
Shaaf, Syed <474256+sshaaf@users.noreply.github.com>
2022-06-14 00:55:31 +0200

    fix: DEFAULT_NULL_TOKEN cannot be resolved or is not a field #5

--- dg8-exercises/src/main/java/org/acme/Person.java
+++ dg8-exercises/src/main/java/org/acme/Person.java
@@ -1,25 +1,23 @@
 package org.acme;
 
-import org.hibernate.search.annotations.Analyze;
-import org.hibernate.search.annotations.Field;
-import org.hibernate.search.annotations.Indexed;
-import org.hibernate.search.annotations.Store;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
 
 @Indexed
 public class Person {
-    @Field(store = Store.YES, analyze = Analyze.NO)
-    String name;
+   @KeywordField
+   String name;
 
-    @Field(store = Store.YES, analyze = Analyze.NO, indexNullAs = Field.DEFAULT_NULL_TOKEN)
-    String surname;
+   @KeywordField
+   String surname;
 
-    public Person(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-    }
+   public Person(String name, String surname) {
+      this.name = name;
+      this.surname = surname;
+   }
 
-    @Override
-    public String toString() {
-        return "Person [name=" + name + ", surname=" + surname + "]";
-    }
+   @Override
+   public String toString() {
+      return "Person [name=" + name + ", surname=" + surname + "]";
+   }
 }
